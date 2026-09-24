d68f67492a44a74971965e80b1a0d65594b7fb37
antoniodcorrea <hello@antoniodiaz.me>
2026-09-24 17:08:01 +0200

    Update 00_match_word_with_wildcard

--- src/05_problems/00_match_word_with_wildcard/implementations/ts/00_match_word_with_wildcard.ts
+++ src/05_problems/00_match_word_with_wildcard/implementations/ts/00_match_word_with_wildcard.ts
@@ -1,47 +1,32 @@
-// d3dog datadog
 export function matcher(pattern: string, data: string): boolean {
-  let n = data.length;
-  let usedWildcards = 0;
-  let patternLength = 0;
+  let patternIndex = 0;
+  let k = 0;
 
-  let i = 0;
-  let j = 0;
+  for (let targetIndex = 0; targetIndex < data.length; targetIndex++) {
+    const patternNumber = Number.parseInt(pattern[patternIndex], 10);
+    const isNumber = !Number.isNaN(patternNumber);
 
-  while (i < pattern.length || j < data.length) {
-    if (i < pattern.length) patternLength++; // Accumulate iterations over pattern.
+    if (isNumber) {
+      k = patternNumber;
+      patternIndex++;
+    }
 
-    const wildcard = parseInt(pattern[i]);
-    const isNumber = typeof wildcard === 'number' && wildcard > 0;
+    while (k > 0) {
+      targetIndex++;
+      k--;
+    }
 
-    // Is a letter, and it does not match, not a word match.
-    if (!isNumber && pattern[i] !== data[j]) {
+    if (targetIndex === data.length) {
+      break;
+    }
+
+    if (pattern[patternIndex] !== data[targetIndex]) {
       return false;
     }
 
-    // Is a letter, advance pattern pointer.
-    if (!isNumber) {
-      i++;
-    }
+    patternIndex++;
 
-    // Is a number, and we do not have wildcards: is a new number, set wildcards
-    if (isNumber && usedWildcards === 0) {
-      usedWildcards = wildcard;
-    }
-
-    // Is a number, and we do have wildcards: consume them and advance if finished.
-    if (isNumber && usedWildcards > 0) {
-      usedWildcards--;
-
-      // Finished with wildcards, advance pattern pointer.
-      if (usedWildcards === 0) {
-        i++;
-      }
-    }
-
-    // Advance data pointer.
-    j++;
   }
 
-  // Return true only if both lengths matches.
-  return patternLength === n;
+  return patternIndex === pattern.length;
 }
